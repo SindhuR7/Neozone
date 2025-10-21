9a3c315d56f4f49505d3f08d1c834fa67ebb90b5
SindhuR7 <alirazasindhu066@gmail.com>
2025-10-21 21:30:14 +0500

    Login Title

--- src/components/LoginTitle.jsx
+++ src/components/LoginTitle.jsx
@@ -5,7 +5,7 @@ const LoginTitle = ({title}) => {
     <>
   <div className='flex flex-col justify-center items-center'>
     <h3 className='font-normal text-2xl text-[#badef0]'>Welcome Back!</h3>
-    <h1 className='font-bold text-white text-[31px]'>{title}</h1>
+    <h1 className='font-bold text-white md:text-[31px] text-2xl'>{title}</h1>
   </div>
     </>
   )
