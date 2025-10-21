f832dad7c6d8e613c899fd6aff7fc0cdf39ef06c
SindhuR7 <alirazasindhu066@gmail.com>
2025-10-21 21:30:28 +0500

    Login Button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const LoginButton = () => {
+const LoginButton = ({typeBtn, width}) => {
   return (
-    <div>LoginButton</div>
+    <>
+    <button 
+    type={typeBtn}
+    className='bg-[#1f999a] rounded-xl h-[48px] md:h-[55px] w-[320px] md:w-[400px] text-white font-bold text-[18px] font-trip-sans cursor-pointer' style={{width:width}}>
+        Login
+    </button>
+    </>
   )
 }
 
