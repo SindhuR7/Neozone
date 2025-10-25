64245ba18bf611f00807b2e12f2e8bc61c19895b
SindhuR7 <alirazasindhu066@gmail.com>
2025-10-25 21:03:22 +0500

    Items

---
+++ src/pages/Items.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import ItemContent from '../components/ItemContent'
+
+const Items = () => {
+  return (
+    <>
+    <div>
+        <Navbar/>
+        <ItemContent/>
+    </div>
+    </>
+  )
+}
+
+export default Items
