61bb7aa0e209c9785abbfae116edc9569822945c
SindhuR7 <alirazasindhu066@gmail.com>
2025-10-25 21:03:03 +0500

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import ProductShow from '../components/ProductShow'
+import MobileApp from '../components/MobileApp'
+import SubFooter from '../components/SubFooter'
+
+const Home = () => {
+  return (
+    <>
+    <Hero/>
+    <ProductShow/>
+    <MobileApp/>
+    <SubFooter/>
+    </>
+  )
+}
+
+export default Home
